ebbcd8480197cd7656d6d972503a748d93a41152
Warren Glasner <wglasner@andrew.cmu.edu>
2019-03-28 17:53:18 -0400

    search functionality

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -1,2 +1,4 @@
 class Flag < ApplicationRecord
+    has_many :flaggings
+    has_many :resources, through: :flaggings
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,10 @@
 class Resource < ApplicationRecord
+    has_many :taggings
+    has_many :tags, through: :taggings
+    has_many :flaggings
+    has_many :flags, through: :flaggings
+
+    def self.search(search)
+        Resource.includes(:tags).where("resources.desc LIKE :search OR tags.name LIKE :search", search: "%#{search}%" ).references(:tags)
+    end
 end
