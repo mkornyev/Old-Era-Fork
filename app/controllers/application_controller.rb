fa369de71411914a6a29c451e05bfcaa9643e31c
SACCFFT <reck98@gmail.com>
2019-04-03 19:40:03 -0400

    pushing to dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+# just show a flash message instead of full CanCan exception
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "You are not authorized to perform this action."
+    redirect_to home_path
+  end
+
+  # handle 404 errors with an exception as well
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    flash[:error] = "I'm sorry, this page does not exist"
+    redirect_to home_path
+  end
   
   private
   # Handling authentication
