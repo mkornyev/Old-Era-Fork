3afbc8646c3fcc30255ca3934792c299fd2dd250
SACCFFT <reck98@gmail.com>
2019-04-09 22:46:28 -0400

    updating status with teammates

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,38 @@
 class TransactionsController < ApplicationController
+
+  def index
+    #Modify so it returns bookmarks based on specific user
+    @transactions = User.all.paginate(page: params[:page]).per_page(15)
+  end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def edit
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    if @transaction.save
+      redirect_to resources_url
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @transaction.update_attributes(transaction_params)
+      redirect_to resources_url
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    if @user.destroy
+    else
+      render action: 'show'
+    end
+  end
 end
